bad90d9469ed57a790f5bb1fb51b71d5f9af3c04
Тимофей <timm09sport@gmail.com>
2022-11-30 17:53:13 +0500

    Я не понимаю что не так написал,не работает.

---
+++ chapter5/task5.6.py
@@ -0,0 +1,13 @@
+age = 54
+if age < 2:
+    print("младенец")
+if age <= 4 or age >2:
+    print("малыш")
+if age >= 13 or age >4:
+    print("ребенок")
+if age >= 20 or age >13:
+    print("подросток")
+if age >=65  or age >20:
+    print("взрослый")
+if age >=65:
+    print("пожилой человек")
