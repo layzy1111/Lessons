95a86bd42e4185e0233552c36e5c5469ebe23e56
Тимофей <timm09sport@gmail.com>
2022-11-30 17:14:30 +0500

    Задание 5.5

--- chapter5/task3.py
+++ chapter5/task3.py
@@ -1,5 +1,7 @@
 color = 'green'
 if color == 'green':
     print("вы заработали 5 очков")
-else:
+if color == 'yellow':
     print("вы заработали 10 очков")
+if color == 'red':
+    print("вы заработали 15 очков")
