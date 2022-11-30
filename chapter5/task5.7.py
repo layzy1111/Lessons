77b38138bf4c8a8f6239c741a6cf91a7963790f5
Тимофей <timm09sport@gmail.com>
2022-11-30 18:08:54 +0500

    Это тоже почему-то не работает.

---
+++ chapter5/task5.7.py
@@ -0,0 +1,11 @@
+favorite_fruits = ['яблоко','банан','груша']
+if favorite_fruits == 'банан':
+    print('ты правда любишь банан!')
+if favorite_fruits == 'апельсин':
+    print('ты правда любишь апельсин!')
+if favorite_fruits == 'яблоко':
+    print('ты правда любишь яблоко!')
+if favorite_fruits == 'ананас':
+    print('ты правда любишь ананас!')
+if favorite_fruits == 'груша':
+    print('ты правда любишь грушу!')
